80d797fc228b4842246d2ba19f5ad29613286623
swetha sayapuraju <swethasayapuraju@gmail.com>
2019-10-03 09:44:24 +0200

    Added files

---
+++ OldPrograms/loopconcepts.java
@@ -0,0 +1,13 @@
+public class loopconcepts {
+    public static void main(String[] args) {
+        for(int i=40;i>30;i--){
+            System.out.println(i);
+        }
+
+
+
+        for(int i=2;i<=12;i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
